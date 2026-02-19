6eb5169413b6ba27ffd364a16801ff9927713f5c
Brian Pereira <74467500+DiscountLogic@users.noreply.github.com>
2026-02-19 18:08:30 -0500

    Update adder.v

--- adder.v
+++ adder.v
@@ -2,7 +2,7 @@
 module adder(A_add, B_add, Result_add);
 
 input [31:0] A_add, B_add;
-output [31:0] Result_add; 
+output [63:0] Result_add; 
 
 reg [31:0] Result_add;
 reg [32:0] LocalCarry;
@@ -11,7 +11,7 @@ integer i;
 
 always@(A_add or B_add)
 begin
-	LocalCarry= 32'd0;
+	LocalCarry= 33'd0;
 	for(i=0; i<32; i=i+1)
 	begin
 		Result_add[i]= A_add[i]^B_add[i]^LocalCarry[i];
